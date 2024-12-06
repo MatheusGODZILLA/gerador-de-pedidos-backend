51e9346360b499a65ad0718166225ba5ee914856
Matheus da Silva <contagodzillajapan@gmail.com>
2024-12-06 03:36:57 -0300

    Feat: adicionando módulo, controlador e serviço de Produtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ProdutosService } from './produtos/produtos.service';
+import { ProdutosController } from './produtos/produtos.controller';
+import { ProdutosModule } from './produtos/produtos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ProdutosModule],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, PrismaService, ProdutosService],
 })
 export class AppModule {}
