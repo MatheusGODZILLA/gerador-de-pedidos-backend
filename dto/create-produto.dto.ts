6976b6f7dcb348763ec566aab18afacb0ec1f256
Matheus da Silva <contagodzillajapan@gmail.com>
2024-12-06 13:55:12 -0300

    Feat: adicionando validações de não vazio

--- dto/create-produto.dto.ts
+++ dto/create-produto.dto.ts
@@ -1,12 +1,15 @@
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
 
 export class CreateProdutoDto {
+  @IsNotEmpty()
   @IsString()
   nome: string;
 
+  @IsNotEmpty()
   @IsNumber()
   preco: number;
 
+  @IsNotEmpty()
   @IsString()
   tag: string;
 }
