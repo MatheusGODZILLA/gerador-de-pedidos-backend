27da65f71eb1e581bd20a92ba71e40f80fc6372a
Matheus da Silva <contagodzillajapan@gmail.com>
2024-12-16 01:53:06 -0300

    Feat: adicionando métodos para obter todos os clientes e por ID

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ClientesService } from './clientes.service';
 
 @Controller('clientes')
-export class ClientesController {}
+export class ClientesController {
+  constructor(private clientesService: ClientesService) {}
+
+  @Get()
+  async getAll() {
+    return this.clientesService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.clientesService.getById(Number(id));
+  }
+}
