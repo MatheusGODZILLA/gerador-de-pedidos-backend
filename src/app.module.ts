f4f7a075a2b5f40c03c9b7481a32871178a258d0
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-15 23:56:07 -0300

    Feat: adicionando módulo de pedidos ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { ProdutosService } from './produtos/produtos.service';
 import { ProdutosController } from './produtos/produtos.controller';
 import { ProdutosModule } from './produtos/produtos.module';
 import { ClientesModule } from './clientes/clientes.module';
+import { PedidosService } from './pedidos/pedidos.service';
+import { PedidosController } from './pedidos/pedidos.controller';
+import { PedidosModule } from './pedidos/pedidos.module';
 
 @Module({
-  imports: [ProdutosModule, ClientesModule],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService, PrismaService, ProdutosService],
+  imports: [ProdutosModule, ClientesModule, PedidosModule],
+  controllers: [AppController, ProdutosController, PedidosController],
+  providers: [AppService, PrismaService, ProdutosService, PedidosService],
 })
 export class AppModule {}
