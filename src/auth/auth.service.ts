ba5f5c816ec9f8df9c3a476994204faccee08745
Matheus da Silva <contagodzillajapan@gmail.com>
2025-02-27 11:01:19 -0300

    Feat: adicionando serviço de autenticação com validação de usuário e geração de token JWT

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, senha: string) {
+    const user = await this.prisma.usuario.findUnique({ where: { email } });
+
+    if (user && (await bcrypt.compare(senha, user.senha))) {
+      return { id: user.id, email: user.email };
+    }
+    throw new UnauthorizedException('Credenciais inválidas');
+  }
+
+  async login(user: { id: number; email: string }) {
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
